25bf71aecf03a68fcc6c1fe023be96199bd90225
Hugo Locurcio <hugo.locurcio@hugo.pro>
2023-05-04 09:13:12 +0200

    Add F3 key to cycle the debug menu between hidden, compact and detailed

--- debug_menu.gd
+++ debug_menu.gd
@@ -23,7 +23,8 @@ extends Control
 @export var information: Label
 @export var settings: Label
 
-# Number of frames to keep in history.
+## The number of frames to keep in history for graph drawing and best/worst calculations.
+## Currently, this also affects how FPS is measured.
 const HISTORY_NUM_FRAMES = 150
 
 const GRAPH_SIZE = Vector2(150, 25)
@@ -31,6 +32,32 @@ const GRAPH_MIN_FPS = 10
 const GRAPH_MAX_FPS = 160
 const GRAPH_MIN_FRAMETIME = 1.0 / GRAPH_MIN_FPS
 const GRAPH_MAX_FRAMETIME = 1.0 / GRAPH_MAX_FPS
+
+## Debug menu display style.
+enum Style {
+	HIDDEN,  ## Debug menu is hidden.
+	VISIBLE_COMPACT,  ## Debug menu is visible, with only the FPS, FPS cap (if any) and time taken to render the last frame.
+	VISIBLE_DETAILED,  ## Debug menu is visible with full information, including graphs.
+	MAX,  ## Represents the size of the Style enum.
+}
+
+## The style to use when drawing the debug menu.
+var style := Style.HIDDEN:
+	set(value):
+		style = value
+		match style:
+			Style.HIDDEN:
+				visible = false
+			Style.VISIBLE_COMPACT, Style.VISIBLE_DETAILED:
+				visible = true
+				frame_number.visible = style == Style.VISIBLE_DETAILED
+				$VBoxContainer/FrameTimeHistory.visible = style == Style.VISIBLE_DETAILED
+				$VBoxContainer/FPSGraph.visible = style == Style.VISIBLE_DETAILED
+				$VBoxContainer/TotalGraph.visible = style == Style.VISIBLE_DETAILED
+				$VBoxContainer/CPUGraph.visible = style == Style.VISIBLE_DETAILED
+				$VBoxContainer/GPUGraph.visible = style == Style.VISIBLE_DETAILED
+				information.visible = style == Style.VISIBLE_DETAILED
+				settings.visible = style == Style.VISIBLE_DETAILED
 
 # Value of `Time.get_ticks_usec()` on the previous frame.
 var last_tick := 0
@@ -51,22 +78,21 @@ var frames_per_second := float(GRAPH_MIN_FPS)
 
 var frame_time_gradient := Gradient.new()
 
+func _init() -> void:
+	# This must be done here instead of `_ready()` to avoid having `visibility_changed` be emitted immediately.
+	visible = false
+
+
 func _ready() -> void:
 	fps_graph.draw.connect(_fps_graph_draw)
 	total_graph.draw.connect(_total_graph_draw)
 	cpu_graph.draw.connect(_cpu_graph_draw)
 	gpu_graph.draw.connect(_gpu_graph_draw)
 
-	# Fill in initial data to prevent graphs from looking strange before `HISTORY_NUM_FRAMES` frames
-	# have been drawn.
 	fps_history.resize(HISTORY_NUM_FRAMES)
-	fps_history.fill(GRAPH_MIN_FPS)
 	frame_history_total.resize(HISTORY_NUM_FRAMES)
-	frame_history_total.fill(GRAPH_MIN_FRAMETIME)
 	frame_history_cpu.resize(HISTORY_NUM_FRAMES)
-	frame_history_cpu.fill(GRAPH_MIN_FRAMETIME)
 	frame_history_gpu.resize(HISTORY_NUM_FRAMES)
-	frame_history_gpu.fill(GRAPH_MIN_FRAMETIME)
 
 	# NOTE: Both FPS and frametimes are colored following FPS logic
 	# (red = 10 FPS, yellow = 60 FPS, green = 110 FPS, cyan = 160 FPS).
@@ -83,6 +109,10 @@ func _ready() -> void:
 	RenderingServer.viewport_set_measure_render_time(get_viewport().get_viewport_rid(), true)
 	update_information_label()
 	update_settings_label()
+
+func _input(event):
+	if event.is_action_pressed(&"cycle_debug_menu"):
+		style = wrapi(style + 1, 0, Style.MAX) as Style
 
 ## Update hardware information label (this can change at runtime based on window size and graphics settings).
 func update_settings_label() -> void:
@@ -252,114 +282,131 @@ func _gpu_graph_draw() -> void:
 
 
 func _process(_delta: float) -> void:
-	fps_graph.queue_redraw()
-	total_graph.queue_redraw()
-	cpu_graph.queue_redraw()
-	gpu_graph.queue_redraw()
+	if visible:
+		fps_graph.queue_redraw()
+		total_graph.queue_redraw()
+		cpu_graph.queue_redraw()
+		gpu_graph.queue_redraw()
 
-	# Difference between the last two rendered frames in milliseconds.
-	var frametime := (Time.get_ticks_usec() - last_tick) * 0.001
+		# Difference between the last two rendered frames in milliseconds.
+		var frametime := (Time.get_ticks_usec() - last_tick) * 0.001
 
-	frame_history_total.push_back(frametime)
-	if frame_history_total.size() > HISTORY_NUM_FRAMES:
-		frame_history_total.pop_front()
+		frame_history_total.push_back(frametime)
+		if frame_history_total.size() > HISTORY_NUM_FRAMES:
+			frame_history_total.pop_front()
 
-	# Frametimes are colored following FPS logic (red = 10 FPS, yellow = 60 FPS, green = 110 FPS, cyan = 160 FPS).
-	# This makes the color gradient non-linear.
-	frametime_avg = frame_history_total.reduce(sum_func) / frame_history_total.size()
-	frame_history_total_avg.text = str(frametime_avg).pad_decimals(2)
-	frame_history_total_avg.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_avg, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		# Frametimes are colored following FPS logic (red = 10 FPS, yellow = 60 FPS, green = 110 FPS, cyan = 160 FPS).
+		# This makes the color gradient non-linear.
+		frametime_avg = frame_history_total.reduce(sum_func) / frame_history_total.size()
+		frame_history_total_avg.text = str(frametime_avg).pad_decimals(2)
+		frame_history_total_avg.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_avg, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	var frametime_min: float = frame_history_total.min()
-	frame_history_total_min.text = str(frametime_min).pad_decimals(2)
-	frame_history_total_min.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_min, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		var frametime_min: float = frame_history_total.min()
+		frame_history_total_min.text = str(frametime_min).pad_decimals(2)
+		frame_history_total_min.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_min, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	var frametime_max: float = frame_history_total.max()
-	frame_history_total_max.text = str(frametime_max).pad_decimals(2)
-	frame_history_total_max.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_max, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		var frametime_max: float = frame_history_total.max()
+		frame_history_total_max.text = str(frametime_max).pad_decimals(2)
+		frame_history_total_max.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_max, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	frame_history_total_last.text = str(frametime).pad_decimals(2)
-	frame_history_total_last.modulate = frame_time_gradient.sample(remap(1000.0 / frametime, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		frame_history_total_last.text = str(frametime).pad_decimals(2)
+		frame_history_total_last.modulate = frame_time_gradient.sample(remap(1000.0 / frametime, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	var viewport_rid := get_viewport().get_viewport_rid()
-	var frametime_cpu := RenderingServer.viewport_get_measured_render_time_cpu(viewport_rid) + RenderingServer.get_frame_setup_time_cpu()
-	frame_history_cpu.push_back(frametime_cpu)
-	if frame_history_cpu.size() > HISTORY_NUM_FRAMES:
-		frame_history_cpu.pop_front()
+		var viewport_rid := get_viewport().get_viewport_rid()
+		var frametime_cpu := RenderingServer.viewport_get_measured_render_time_cpu(viewport_rid) + RenderingServer.get_frame_setup_time_cpu()
+		frame_history_cpu.push_back(frametime_cpu)
+		if frame_history_cpu.size() > HISTORY_NUM_FRAMES:
+			frame_history_cpu.pop_front()
 
-	frametime_cpu_avg = frame_history_cpu.reduce(sum_func) / frame_history_cpu.size()
-	frame_history_cpu_avg.text = str(frametime_cpu_avg).pad_decimals(2)
-	frame_history_cpu_avg.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_cpu_avg, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		frametime_cpu_avg = frame_history_cpu.reduce(sum_func) / frame_history_cpu.size()
+		frame_history_cpu_avg.text = str(frametime_cpu_avg).pad_decimals(2)
+		frame_history_cpu_avg.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_cpu_avg, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	var frametime_cpu_min: float = frame_history_cpu.min()
-	frame_history_cpu_min.text = str(frametime_cpu_min).pad_decimals(2)
-	frame_history_cpu_min.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_cpu_min, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		var frametime_cpu_min: float = frame_history_cpu.min()
+		frame_history_cpu_min.text = str(frametime_cpu_min).pad_decimals(2)
+		frame_history_cpu_min.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_cpu_min, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	var frametime_cpu_max: float = frame_history_cpu.max()
-	frame_history_cpu_max.text = str(frametime_cpu_max).pad_decimals(2)
-	frame_history_cpu_max.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_cpu_max, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		var frametime_cpu_max: float = frame_history_cpu.max()
+		frame_history_cpu_max.text = str(frametime_cpu_max).pad_decimals(2)
+		frame_history_cpu_max.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_cpu_max, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	frame_history_cpu_last.text = str(frametime_cpu).pad_decimals(2)
-	frame_history_cpu_last.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_cpu, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		frame_history_cpu_last.text = str(frametime_cpu).pad_decimals(2)
+		frame_history_cpu_last.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_cpu, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	var frametime_gpu := RenderingServer.viewport_get_measured_render_time_gpu(viewport_rid)
-	frame_history_gpu.push_back(frametime_gpu)
-	if frame_history_gpu.size() > HISTORY_NUM_FRAMES:
-		frame_history_gpu.pop_front()
+		var frametime_gpu := RenderingServer.viewport_get_measured_render_time_gpu(viewport_rid)
+		frame_history_gpu.push_back(frametime_gpu)
+		if frame_history_gpu.size() > HISTORY_NUM_FRAMES:
+			frame_history_gpu.pop_front()
 
-	frametime_gpu_avg = frame_history_gpu.reduce(sum_func) / frame_history_gpu.size()
-	frame_history_gpu_avg.text = str(frametime_gpu_avg).pad_decimals(2)
-	frame_history_gpu_avg.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_gpu_avg, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		frametime_gpu_avg = frame_history_gpu.reduce(sum_func) / frame_history_gpu.size()
+		frame_history_gpu_avg.text = str(frametime_gpu_avg).pad_decimals(2)
+		frame_history_gpu_avg.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_gpu_avg, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	var frametime_gpu_min: float = frame_history_gpu.min()
-	frame_history_gpu_min.text = str(frametime_gpu_min).pad_decimals(2)
-	frame_history_gpu_min.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_gpu_min, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		var frametime_gpu_min: float = frame_history_gpu.min()
+		frame_history_gpu_min.text = str(frametime_gpu_min).pad_decimals(2)
+		frame_history_gpu_min.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_gpu_min, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	var frametime_gpu_max: float = frame_history_gpu.max()
-	frame_history_gpu_max.text = str(frametime_gpu_max).pad_decimals(2)
-	frame_history_gpu_max.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_gpu_max, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		var frametime_gpu_max: float = frame_history_gpu.max()
+		frame_history_gpu_max.text = str(frametime_gpu_max).pad_decimals(2)
+		frame_history_gpu_max.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_gpu_max, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	frame_history_gpu_last.text = str(frametime_gpu).pad_decimals(2)
-	frame_history_gpu_last.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_gpu, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		frame_history_gpu_last.text = str(frametime_gpu).pad_decimals(2)
+		frame_history_gpu_last.modulate = frame_time_gradient.sample(remap(1000.0 / frametime_gpu, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
 
-	frames_per_second = 1000.0 / frametime_avg
-	fps_history.push_back(frames_per_second)
-	if fps_history.size() > HISTORY_NUM_FRAMES:
-		fps_history.pop_front()
+		frames_per_second = 1000.0 / frametime_avg
+		fps_history.push_back(frames_per_second)
+		if fps_history.size() > HISTORY_NUM_FRAMES:
+			fps_history.pop_front()
 
-	fps.text = str(floor(frames_per_second)) + " FPS"
-	var frame_time_color := frame_time_gradient.sample(remap(frames_per_second, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
-	fps.modulate = frame_time_color
+		fps.text = str(floor(frames_per_second)) + " FPS"
+		var frame_time_color := frame_time_gradient.sample(remap(frames_per_second, GRAPH_MIN_FPS, GRAPH_MAX_FPS, 0.0, 1.0))
+		fps.modulate = frame_time_color
 
-	frame_time.text = str(frametime).pad_decimals(2) + " mspf"
-	frame_time.modulate = frame_time_color
+		frame_time.text = str(frametime).pad_decimals(2) + " mspf"
+		frame_time.modulate = frame_time_color
 
-	var vsync_string := ""
-	match DisplayServer.window_get_vsync_mode():
-		DisplayServer.VSYNC_ENABLED:
-			vsync_string = "V-Sync"
-		DisplayServer.VSYNC_ADAPTIVE:
-			vsync_string = "Adaptive V-Sync"
-		DisplayServer.VSYNC_MAILBOX:
-			vsync_string = "Mailbox V-Sync"
+		var vsync_string := ""
+		match DisplayServer.window_get_vsync_mode():
+			DisplayServer.VSYNC_ENABLED:
+				vsync_string = "V-Sync"
+			DisplayServer.VSYNC_ADAPTIVE:
+				vsync_string = "Adaptive V-Sync"
+			DisplayServer.VSYNC_MAILBOX:
+				vsync_string = "Mailbox V-Sync"
 
-	if Engine.max_fps > 0 or OS.low_processor_usage_mode:
-		# Display FPS cap determined by `Engine.max_fps` or low-processor usage mode sleep duration
-		# (the lowest FPS cap is used).
-		var low_processor_max_fps := roundf(1000000.0 / OS.low_processor_usage_mode_sleep_usec)
-		var fps_cap := low_processor_max_fps
-		if Engine.max_fps > 0:
-			fps_cap = min(Engine.max_fps, low_processor_max_fps)
-		frame_time.text += " (cap: " + str(fps_cap) + " FPS"
+		if Engine.max_fps > 0 or OS.low_processor_usage_mode:
+			# Display FPS cap determined by `Engine.max_fps` or low-processor usage mode sleep duration
+			# (the lowest FPS cap is used).
+			var low_processor_max_fps := roundi(1000000.0 / OS.low_processor_usage_mode_sleep_usec)
+			var fps_cap := low_processor_max_fps
+			if Engine.max_fps > 0:
+				fps_cap = mini(Engine.max_fps, low_processor_max_fps)
+			frame_time.text += " (cap: " + str(fps_cap) + " FPS"
 
-		if not vsync_string.is_empty():
-			frame_time.text += " + " + vsync_string
+			if not vsync_string.is_empty():
+				frame_time.text += " + " + vsync_string
 
-		frame_time.text += ")"
-	else:
-		if not vsync_string.is_empty():
-			frame_time.text += " (" + vsync_string + ")"
+			frame_time.text += ")"
+		else:
+			if not vsync_string.is_empty():
+				frame_time.text += " (" + vsync_string + ")"
 
-	frame_number.text = "Frame: " + str(Engine.get_frames_drawn())
+		frame_number.text = "Frame: " + str(Engine.get_frames_drawn())
 
 	last_tick = Time.get_ticks_usec()
+
+
+func _on_visibility_changed() -> void:
+	if visible:
+		# Reset graphs to prevent them from looking strange before `HISTORY_NUM_FRAMES` frames
+		# have been drawn.
+		var frametime_last := (Time.get_ticks_usec() - last_tick) * 0.001
+		fps_history.resize(HISTORY_NUM_FRAMES)
+		fps_history.fill(1000.0 / frametime_last)
+		frame_history_total.resize(HISTORY_NUM_FRAMES)
+		frame_history_total.fill(frametime_last)
+		frame_history_cpu.resize(HISTORY_NUM_FRAMES)
+		var viewport_rid := get_viewport().get_viewport_rid()
+		frame_history_cpu.fill(RenderingServer.viewport_get_measured_render_time_cpu(viewport_rid) + RenderingServer.get_frame_setup_time_cpu())
+		frame_history_gpu.resize(HISTORY_NUM_FRAMES)
+		frame_history_gpu.fill(RenderingServer.viewport_get_measured_render_time_gpu(viewport_rid))
