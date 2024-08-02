402e4306f09c2e4cab879b1fc20d6b4b7f4026a4
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-08-02 16:55:25 +0200

    Fix framerate reporting when the game is paused
    Previously, framerates saw a sudden drop when the game was paused,
    as the debug menu was also paused.
    
    This also adds a pause toggle to the demo so this behavior can
    be showcased.

--- test.gd
+++ test.gd
@@ -2,4 +2,9 @@ extends Node3D
 
 
 func _on_cycle_debug_menu_display_mode_pressed() -> void:
+	@warning_ignore("int_as_enum_without_cast")
 	DebugMenu.style = wrapi(DebugMenu.style + 1, 0, DebugMenu.Style.MAX)
+
+
+func _on_pause_toggled(toggled_on: bool) -> void:
+	get_tree().paused = toggled_on
